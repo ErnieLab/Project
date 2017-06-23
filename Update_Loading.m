4d8080d5439b375147404477a33610582f8f1c39
ErnieLab <susanernie@outlook.com>
2017-06-23 23:14:24 +0800

    make it simple

--- Update_Loading.m
+++ Update_Loading.m
@@ -8,19 +8,11 @@ for BS_index = 1:1:n_BS
     RB_used_Num = 0;
 
     if BS_index <= n_MC
-	    for RB_index = 1:1:n_ttoffered
-		    if BS_RB_table(BS_index, RB_index) == 1
-			    RB_used_Num  = RB_used_Num + 1;
-		    end
-	    end
+    	RB_used_Num = length(nonzeros(BS_RB_table(BS_index, 1:n_ttoffered)))
 
 	    Load_TST(BS_index) = RB_used_Num/n_ttoffered;
 	else
-	    for RB_index = 1:1:Pico_part
-		    if BS_RB_table(BS_index, RB_index) == 1
-			    RB_used_Num  = RB_used_Num + 1;
-		    end
-	    end
+		RB_used_Num = length(nonzeros(BS_RB_table(BS_index, 1:Pico_part)))
 
 	    Load_TST(BS_index) = RB_used_Num/Pico_part;
 	end
