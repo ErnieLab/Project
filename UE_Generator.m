65c0c20dc14d208ef93737a4eea06ae1fe26c3f8
ErnieLab <susanernie@outlook.com>
2017-07-11 13:52:03 +0800

    for simu

--- UE_Generator.m
+++ UE_Generator.m
@@ -7,20 +7,20 @@
 % System Initialize %
 % ----------------- %
 edge = 4763;       % Á≥ªÁµ±?ÑÈ???[m]
-n_UE = 300;        % UE ?ÑÊï∏??
+n_UE = 400;        % UE ?ÑÊï∏??
 
 
 % ----------- %
 % ‰ΩçÁΩÆ?Ø‰??? %
 % ----------- %
-UE_location = zeros(n_UE, 2); % Pico Cell ?Ñ‰?ÁΩ?
-
-x = (rand(n_UE,1)-0.5)*edge;
-y = (rand(n_UE,1)-0.5)*edge;
-
-UE_location = [x,y];
-
-save UE_lct_n300_random UE_location
+% UE_location = zeros(n_UE, 2); % Pico Cell ?Ñ‰?ÁΩ?
+% 
+% x = (rand(n_UE,1)-0.5)*edge;
+% y = (rand(n_UE,1)-0.5)*edge;
+% 
+% UE_location = [x,y];
+% 
+% save UE_lct_n300_random UE_location
 
 
 
@@ -28,27 +28,27 @@ save UE_lct_n300_random UE_location
 % ----------------- %
 % ?Ü‰∏≠?®‰∏≠?ìÁ??∫Ê?  %
 % ----------------- %
-% UE_location = zeros(n_UE, 2); % Pico Cell ?Ñ‰?ÁΩ?
-% 
-% central_part = 10;  % ?ÜÊ???0 
-% % EX:
-% %    central_part = 5
-% %    Ë°®Á§∫??/10?ÑUE?∫Âú®‰∏≠È?
-% 
-% % UE??/4?∫Âú®‰∏≠È?
-% x_1 = (rand(n_UE*central_part/10,1)-0.5)*(edge/2);
-% y_1 = (rand(n_UE*central_part/10,1)-0.5)*(edge/2);
-% 
-% 
-% x_2 = (rand(n_UE*(10 - central_part)/10,1)-0.5)*(edge);
-% y_2 = (rand(n_UE*(10 - central_part)/10,1)-0.5)*(edge);
-% 
-% x   = [x_1; x_2];
-% y   = [y_1; y_2];
-% 
-% UE_location = [x,y];
-% 
-% save UE_lct_n400_centralize UE_location
+UE_location = zeros(n_UE, 2); % Pico Cell ?Ñ‰?ÁΩ?
+
+central_part = 8;  % ?ÜÊ???0 
+% EX:
+%    central_part = 5
+%    Ë°®Á§∫??/10?ÑUE?∫Âú®‰∏≠È?
+
+% UE??/4?∫Âú®‰∏≠È?
+x_1 = (rand(n_UE*central_part/10,1)-0.5)*(edge/2);
+y_1 = (rand(n_UE*central_part/10,1)-0.5)*(edge/2);
+
+
+x_2 = (rand(n_UE*(10 - central_part)/10,1)-0.5)*(edge);
+y_2 = (rand(n_UE*(10 - central_part)/10,1)-0.5)*(edge);
+
+x   = [x_1; x_2];
+y   = [y_1; y_2];
+
+UE_location = [x,y];
+
+save UE_lct_n400_centralize UE_location
 
 
 
