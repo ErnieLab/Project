8e351a7db0d95b1335220b1d866fb06f1c381da8
ErnieLab <susanernie@outlook.com>
2017-07-11 13:50:05 +0800

    ok

--- Update_Loading.m
+++ Update_Loading.m
@@ -1,8 +1,9 @@
 % ============================================== %
 % 該function是用來更新全部Base  Station的Loading %
 % ============================================== %
-function [Load_TST] = Update_Loading(n_BS, n_MC, BS_RB_table, n_ttoffered, Pico_part)
+function [Load_TST_output] = Update_Loading(n_BS, n_MC, BS_RB_table, n_ttoffered, Pico_part)
 
+Load_table = zeros(1, n_BS);
 
 for BS_index = 1:1:n_BS
     RB_used_Num = 0;
@@ -10,10 +11,12 @@ for BS_index = 1:1:n_BS
     if BS_index <= n_MC
     	RB_used_Num = length(nonzeros(BS_RB_table(BS_index, 1:n_ttoffered)));
 
-	    Load_TST(BS_index) = RB_used_Num/n_ttoffered;
+	    Load_table(BS_index) = RB_used_Num/n_ttoffered;
 	else
 		RB_used_Num = length(nonzeros(BS_RB_table(BS_index, 1:Pico_part)));
 
-	    Load_TST(BS_index) = RB_used_Num/Pico_part;
+	    Load_table(BS_index) = RB_used_Num/Pico_part;
 	end
 end
+
+Load_TST_output = Load_table;
