359fcd442173b6a261ceba58a72ce3dbb69621af
ErnieLab <susanernie@outlook.com>
2017-07-11 13:51:42 +0800

    for simu

--- Ben/Update_Loading.m
+++ Ben/Update_Loading.m
@@ -1,27 +1,22 @@
 % ============================================== %
 % 該function是用來更新全部Base  Station的Loading %
 % ============================================== %
-function [Load_TST] = Update_Loading(n_BS, n_MC, BS_RB_table, n_ttoffered, Pico_part)
+function [Load_TST_output] = Update_Loading(n_BS, n_MC, BS_RB_table, n_ttoffered, Pico_part)
 
+Load_table = zeros(1, n_BS);
 
 for BS_index = 1:1:n_BS
     RB_used_Num = 0;
 
     if BS_index <= n_MC
-	    for RB_index = 1:1:n_ttoffered
-		    if BS_RB_table(BS_index, RB_index) == 1
-			    RB_used_Num  = RB_used_Num + 1;
-		    end
-	    end
+    	RB_used_Num = length(nonzeros(BS_RB_table(BS_index, 1:n_ttoffered)));
 
-	    Load_TST(BS_index) = RB_used_Num/n_ttoffered;
+	    Load_table(BS_index) = RB_used_Num/n_ttoffered;
 	else
-	    for RB_index = 1:1:Pico_part
-		    if BS_RB_table(BS_index, RB_index) == 1
-			    RB_used_Num  = RB_used_Num + 1;
-		    end
-	    end
+		RB_used_Num = length(nonzeros(BS_RB_table(BS_index, 1:Pico_part)));
 
-	    Load_TST(BS_index) = RB_used_Num/Pico_part;
+	    Load_table(BS_index) = RB_used_Num/Pico_part;
 	end
 end
+
+Load_TST_output = Load_table;
